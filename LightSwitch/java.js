8d6176c2dcd14ca1946963e31e6d4c765327c1ca
Jehkk <nhgameboy@msn.com>
2017-10-03 20:08:23 -0400

    Add files via upload

---
+++ LightSwitch/java.js
@@ -0,0 +1,13 @@
+function turnOn(){
+	document.body.style.backgroundColor = "white";
+	document.getElementById("on").style.backgroundColor = "gray";
+	document.getElementById("off").style.backgroundColor = "white";
+	
+}
+
+function turnOff(){
+	document.body.style.backgroundColor = "black";
+	document.getElementById("off").style.backgroundColor = "gray";
+	document.getElementById("on").style.backgroundColor = "white";
+
+}
